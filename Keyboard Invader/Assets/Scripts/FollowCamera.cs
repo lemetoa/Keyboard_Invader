9606c02fef68b68682e7ce7c092d48e47c9d2f5c
minyoungdoo <dudenx01@naver.com>
2021-10-07 12:05:22 +0900

    드래그앤 드랍, 카메라
    - 키보드 물리 효과
    - 키보드 드래그앤드랍
    - 카메라가 플레이어 따라가게

---
+++ Keyboard Invader/Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 playerPos = player.transform.position;
+        transform.position = new Vector3(playerPos.x, playerPos.y, this.transform.position.z);
+    }
+}
